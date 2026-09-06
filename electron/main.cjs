c2fcbc289f383f73fbf0a1ed1f6f28994f8a5555
khrislloyd18-ops <khrislloyd18@gmail.com>
2026-09-06 12:09:47 +0800

    add electron desktop app support

---
+++ electron/main.cjs
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const isDevelopment = !app.isPackaged
+
+app.disableHardwareAcceleration()
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 1440,
+    height: 920,
+    minWidth: 1024,
+    minHeight: 700,
+    title: 'Smart Blood Donation',
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      preload: path.join(__dirname, 'preload.cjs'),
+    },
+  })
+
+  if (isDevelopment) {
+    window.loadURL('http://localhost:3000')
+  } else {
+    window.loadFile(path.join(__dirname, '..', 'dist', 'index.html'))
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
